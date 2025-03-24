2ff2d374bc998c4bad39a26282e3f5777864e180
tarasmatvienko <matviyenkot@gmail.com>
2025-03-24 18:52:44 +0200

    task09 fix01

--- task09/jsrc/main/java/com/task09/ApiHandler.java
+++ task09/jsrc/main/java/com/task09/ApiHandler.java
@@ -25,7 +25,8 @@ import java.util.Map;
 	roleName = "api_handler-role",
 	isPublishVersion = true,
 	aliasName = "${lambdas_alias_name}",
-	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED,
+	layers = {"weather_sdk"}
 )
 @LambdaUrlConfig(
 		authType = AuthType.NONE,
